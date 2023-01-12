0e592d429be037c513899fa0f64b8d7889ea3053
Максим Процеров <makslive@bk.ru>
2023-01-12 03:21:24 +0300

    firs

--- yatube_api/api/admin.py
+++ yatube_api/api/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Follow, Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'author', 'post', 'created')
+    search_fields = ('text',)
+    list_filter = ('created',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    search_fields = ('user__username', 'following__username')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Follow, FollowAdmin)
---
+++ yatube_api/api/models.py
@@ -0,0 +1,73 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.title
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='follower',
+                             verbose_name='Подписчик')
+    following = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  related_name='following',
+                                  verbose_name='Автор')
+
+    class Meta:
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'following'), name='unique_subscription'
+            )
+        ]
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата публикации'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(Group, related_name='posts',
+                              on_delete=models.SET_NULL, blank=True,
+                              null=True, verbose_name='Группа')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments',
+        verbose_name='Автор'
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments',
+        verbose_name='Пост'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    created = models.DateTimeField(
+        auto_now_add=True, db_index=True,
+        verbose_name='Дата комментария'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
